4273ccbbd231934dad1c5cb15aee76f72158ee7d
Aleksandr Maevskiy <aleks.maewsky@yandex.ru>
2023-11-12 12:02:57 +0300

    bmi index

---
+++ src/BmiService.java
@@ -0,0 +1,16 @@
+public class BmiService {
+    public int calculate(float heightM, int weightKg) {
+        float indexKg;
+        if (heightM * heightM >= 1) {
+            indexKg = (weightKg / (heightM * heightM));
+        }else {
+            indexKg = 10;
+
+        }
+        return (int) indexKg;
+
+    }
+
+
+}
+
