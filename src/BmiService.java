c2cfb5639dd53786da325dbe27f43218e2dc5031
Aleksandr Maevskiy <aleks.maewsky@yandex.ru>
2023-11-14 18:35:05 +0300

    FIx code 2.0

--- src/BmiService.java
+++ src/BmiService.java
@@ -1,8 +1,7 @@
 public class BmiService {
     public int calculate(float heightM, int weightKg) {
         float indexKg = ((weightKg / (heightM * heightM)));
-        if (heightM * heightM > 0) {
-        }
+
         return (int) indexKg;
 
     }
