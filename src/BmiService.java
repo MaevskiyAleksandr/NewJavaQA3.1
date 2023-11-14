65792658ab3f9d24762ce014bfb85483b54891b6
Aleksandr Maevskiy <aleks.maewsky@yandex.ru>
2023-11-14 18:08:56 +0300

    New Fix code

--- src/BmiService.java
+++ src/BmiService.java
@@ -1,9 +1,7 @@
 public class BmiService {
     public int calculate(float heightM, int weightKg) {
         float indexKg = ((weightKg / (heightM * heightM)));
-        if (heightM * heightM >= 1) {
-            indexKg = (weightKg / (heightM * heightM));
-
+        if (heightM * heightM > 0) {
         }
         return (int) indexKg;
 
